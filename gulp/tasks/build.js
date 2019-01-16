c41dcab914840d80e823d91c8cb1e9d306ec8c2a
Gabor Bach <gaborbach@gmail.com>
2019-01-15 19:47:12 -0600

    completed gulp build test

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,9 +1,37 @@
 var gulp = require('gulp'),
 imagemin = require('gulp-imagemin'),
-del = require('del');
+del = require('del'),
+usemin = require('gulp-usemin'),
+rev = require('gulp-rev'),
+cssnano = require('gulp-cssnano'),
+uglify = require('gulp-uglify'),
+browserSync = require('browser-sync').create();
 
-gulp.task('deleteDistFolder', function() {
-    return del("./dist");
+gulp.task('previewDist', function() {
+    browserSync.init({
+        notify: false,
+        server: {
+          baseDir: "docs"
+        }
+      });
+});
+
+gulp.task('deleteDistFolder', ['icons'], function() {
+    return del("./docs");
+});
+
+gulp.task('copyGeneralFiles', ['deleteDistFolder'], function() {
+    var pathsToCopy = [
+        './app/**/*', 
+        '!./app/index.html',
+        '!./app/assets/images/**',
+        '!./app/assets/styles/**',
+        '!./app/assets/scripts/**',
+        '!./app/temp',
+        '!./app/temp/**'
+    ]
+    return gulp.src(pathsToCopy)
+    .pipe(gulp.dest("./docs"));
 });
 
 gulp.task('optimizeImages', ['deleteDistFolder'], function() {
@@ -13,8 +41,21 @@ gulp.task('optimizeImages', ['deleteDistFolder'], function() {
         interlaced: true,
         multipass: true
     }))    
-    .pipe(gulp.dest("./dist/assets/images"));
+    .pipe(gulp.dest("./docs/assets/images"));
     
 });
 
-gulp.task('build', ['deleteDistFolder', 'optimizeImages']);
+gulp.task('useminTrigger', ['deleteDistFolder'], function(){
+    gulp.setMaxListeners("usemin");
+});
+
+gulp.task('usemin', ['styles', 'scripts'], function() {
+    return gulp.src('./app/index.html')
+    .pipe(usemin({
+        css: [function() {return rev()}, function() {return cssnano()}],
+        js: [function() {return rev()}, function() {return uglify()}]
+    }))
+    .pipe(gulp.dest("./docs"));
+});
+
+gulp.task('build', ['deleteDistFolder', 'copyGeneralFiles', 'optimizeImages', 'useminTrigger']);
